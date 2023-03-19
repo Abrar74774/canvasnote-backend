44f4e97fd83b0da1853ea20ff62af61a9d6c89d0
Abrar74774 <abrarshahriarhossain@gmail.com>
2023-03-19 13:09:27 +0300

    Add cors whitelist

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import cors from 'cors';
+
 const app = express();
 const port = process.env.PORT || 8080;
+const whitelist = [process.env.TEST_IP, 'https://abrar74774.github.io/canvasnote']
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+    }
+}));
 
 app.get('/', function (req, res) {
     return res.send('Backend in development');
