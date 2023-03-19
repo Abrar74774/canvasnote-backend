731f7966b214b39c15cbdfbc818fac62ff5733c6
Abrar74774 <abrarshahriarhossain@gmail.com>
2023-03-19 13:31:51 +0300

    Enable cors for all origins

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const app = express();
 const port = process.env.PORT || 8080;
 const whitelist = ['http://localhost:5173/canvasnote/', 'https://abrar74774.github.io/canvasnote']
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true)
-        } else {
-          callback(new Error('Not allowed by CORS'))
-        }
-    }
-}));
+app.use(cors());
+    // {
+    // origin: function (origin, callback) {
+    //     if (whitelist.indexOf(origin) !== -1) {
+    //       callback(null, true)
+    //     } else {
+    //       callback(new Error('Not allowed by CORS'))
+    //     }
+    // }
+// }
 
 app.get('/', function (req, res) {
     return res.send('Backend in development');
